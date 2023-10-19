d4489de99142ba4c4459e00571686341ec6b05aa
DecryptLife <benson.thomas2602@gmail.com>
2023-10-19 00:55:58 -0500

    improving code

--- lively_backend/index.js
+++ lively_backend/index.js
@@ -7,136 +7,51 @@ const PORT = process.env.PORT || 3001;
 const app = express();
 const cookieParser = require("cookie-parser");
 const mongoose = require("mongoose");
-const userSchema = require("./src/userSchema");
 const auth = require("./src/auth");
 const articles = require("./articles");
-const User = mongoose.model("user", userSchema);
-const path = require("path");
-const { type } = require("os");
-var session = require("express-session");
-var passport = require("passport");
-const router = express.Router();
-var GoogleStrategy = require("passport-google-oauth").OAuth2Strategy;
 const cors = require("cors");
 
 const corsOptions = {
   origin: "http://localhost:3000",
-  // preflightContinue: false,
-  // origin: "*",
   "Access-Control-Allow-Origin": "*",
   credentials: true,
 };
 
-const connectionString =
-  "mongodb+srv://bt22:xQ8BdgzsZt1f1iwc@cluster0.9auii05.mongodb.net/lively?retryWrites=true&w=majority";
+const connectionString = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.9auii05.mongodb.net/lively?retryWrites=true&w=majority`;
+// "mongodb+srv://bt22:xQ8BdgzsZt1f1iwc@cluster0.9auii05.mongodb.net/lively?retryWrites=true&w=majority";
 app.use(express.json({ limit: "50mb" }));
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(cors(corsOptions));
 
-app.use(
-  session({
-    secret: "doNotGuessTheSecret",
-    resave: false,
-    saveUninitialized: true,
-  })
-);
-
-app.use(passport.initialize());
-app.use(passport.session());
-
-passport.serializeUser(function (user, done) {
-  done(null, user);
+app.use((req, res, next) => {
+  res.header("Content-Type", "application/json;charset=UTF-8");
+  res.header("Access-Control-Allow-Credentials", true);
+  res.header(
+    "Access-Control-Allow-Headers",
+    "Origin, X-Requested-With, Content-Type, Accept"
+  );
+  next();
 });
 
-passport.deserializeUser(function (user, done) {
-  done(null, user);
+console.log("check");
+
+mongoose.connect(connectionString, {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+  dbName: "lively",
 });
 
-passport.use(
-  new GoogleStrategy(
-    {
-      clientID: process.env["GOOGLE_CLIENT_ID"],
-      clientSecret: process.env["GOOGLE_CLIENT_SECRET"],
-      callbackURL: "/auth/google/callback",
-    },
-    function (accessToken, refreshToken, profile, done) {
-      console.log("chesshkjshk");
-      let user = {
-        email: profile.emails[0].value,
-        name: profile.name.givenName + " " + profile.name.familyName,
-        id: profile.id,
-        token: accessToken,
-      };
-      console.log(accessToken);
-      console.log(refreshToken);
-      User.findOne({ email: user.email }, (err, docs) => {
-        if (err) {
-          console.log(err);
-        } else {
-          if (docs) {
-            return done(null, user);
-          } else {
-            return done(null, user);
-          }
-          console.log("DOCS: ", docs);
-        }
-      });
+app.use((err, req, res, next) => {
+  console.error(err.stack); // Log the stack trace for debugging purposes
+  res.status(500).send("Internal Server Error"); // Send an appropriate error response
+});
 
-      console.log(user);
-      // You can perform any necessary actions with your user at this point,
-      // e.g. internal verification against a users table,
-      // creating new user entries, etc.
-      console.log("wtf is happening");
-
-      // User.findOrCreate(..., function(err, user) {
-      //     if (err) { return done(err); }
-      //     done(null, user);
-      // });
-    }
-  )
-);
-// Redirect the user to Google for authentication.  When complete,
-// Google will redirect the user back to the application at
-//     /auth/google/callback
-app.get(
-  "/auth/google",
-  passport.authenticate("google", { scope: ["email", "profile"] }),
-  function (err, user, info) {
-    if (err) {
-      return next(err);
-    }
-    if (!user) {
-      return res.redirect("/");
-    }
-
-    // req / res held in closure
-    req.logIn(user, function (err) {
-      if (err) {
-        return next(err);
-      }
-      return res.send(user);
-    });
-  }
-); // could have a passport auth second arg {scope: 'email'}
-
-// Google will redirect the user to this URL after approval.  Finish the
-// authentication process by attempting to obtain an access token.  If
-// access was granted, the user will be logged in.  Otherwise,
-// authentication has failed.
-app.get(
-  "/auth/google/callback",
-
-  passport.authenticate("google", {
-    successRedirect: "http://localhost:3000/home",
-    failureRedirect: "/",
-  })
-);
-
+console.log("check 2");
 auth(app);
-profile(app);
-articles(app);
-following(app);
+// profile(app);
+// articles(app);
+// following(app);
 
 app.listen(PORT, () => {
   console.log(`Server listening on ${PORT}`);
--- lively_backend/src/auth.js
+++ lively_backend/src/auth.js
@@ -1,21 +1,19 @@
+require("dotenv").config();
+const express = require("express");
 const mongoose = require("mongoose");
 const userSchema = require("./userSchema");
 const profileSchema = require("./profileSchema");
 const Profile = mongoose.model("profile", profileSchema);
 const User = mongoose.model("user", userSchema);
-const connectionString =
-  "mongodb+srv://bt22:xQ8BdgzsZt1f1iwc@cluster0.9auii05.mongodb.net/lively?retryWrites=true&w=majority";
-const REDIS_URL =
-  "redis://:pd2ab0292dfca79eb5c1cbd30297384c1ed16e10da40daf823b0f92562eca1310@ec2-3-230-185-236.compute-1.amazonaws.com:19529";
-const redis = require("redis").createClient(REDIS_URL);
 
 let sessionUser = {};
 let cookieKey = "sid";
 let userObjs = {};
+const router = express.Router();
 
 const md5 = require("md5");
 
-function isLoggedIn(req, res, next) {
+router.use((req, res, next) => {
   console.log("Authenticated: ", req.isAuthenticated());
   if (!req.cookies) {
     return res.sendStatus(401);
@@ -40,9 +38,9 @@ function isLoggedIn(req, res, next) {
       return res.sendStatus(401);
     }
   }
-}
+});
 
-function register(req, res) {
+router.post("/register", async (req, res) => {
   let username = req.body.username;
   let email = req.body.email;
   let headline = "Please update your headline";
@@ -56,196 +54,162 @@ function register(req, res) {
 
   if (!avatar) avatar = "https://www.gstatic.com/webp/gallery3/1.sm.png";
   let salt = username + "lively";
-  // var date = new Date(dob);
-  // var ms = date.getTime().toString();
   let hash = md5(salt + password);
 
-  (async () => {
-    const connector = mongoose.connect(connectionString, {
-      useNewUrlParser: true,
-      useUnifiedTopology: true,
+  let createdTime = new Date().getTime();
+
+  const user = await User.findOne({ username });
+
+  if (user) {
+    res.status(403).json({ message: "User already exists" });
+  } else {
+    const newUser = new User({ username, hash, email, createdTime });
+    await newUser.save();
+    const newUserProfile = new Profile({
+      username,
+      email,
+      headline,
+      mobile,
+      dob,
+      zipcode,
+      avatar,
+      createdTime,
     });
-
-    var createdTime = new Date().getTime();
-
-    let user = new User({
-      username: username,
-      password: hash,
-      email: email,
-      created: createdTime,
-    });
-
-    let user_profile = new Profile({
-      username: username,
-      email: email,
-      headline: headline,
-      mobile: mobile,
-      dob: dob,
-      zipcode: zipcode,
-      avatar: avatar,
-      created: createdTime,
-    });
-
-    await connector.then(() => {
-      User.findOne({ username: user["username"] }, (err, docs) => {
-        if (err) {
-          console.log(err);
-        } else {
-          if (docs) {
-            return res
-              .status(200)
-              .send({ msg: "User with this username already exists" });
-          } else {
-            user.save((err, docs) => {
-              if (err) {
-                console.log(err);
-              } else {
-                console.log("User added to database successfully");
-                user_profile.save((err, docs) => {
-                  if (err) {
-                    console.log(err);
-                  } else {
-                    console.log("Profile created for user");
-                  }
-                });
-
-                let msg = { result: "success", username: username };
-                res.send(msg);
-              }
-            });
-          }
-        }
-      });
-    });
-  })();
-}
-
-const login = (req, res) => {
-  console.log("logging in");
-  let exists_sid = req.cookies[cookieKey];
-
-  let username = req.body.username;
-  let password = req.body.password;
-
-  if (!username || !password)
-    return res.status(400).send("Missing username or password");
-
-  let user = userObjs[username];
-  console.log("Jack: ", user);
-  (async () => {
-    const connector = mongoose.connect(connectionString, {
-      useNewUrlParser: true,
-      useUnifiedTopology: true,
-    });
-
-    await connector.then(() => {
-      // salt the password
-      // check if the username and password matches
-      let salt = username + "lively";
-      let hash = md5(salt + password);
-
-      User.findOne({ username: username }, (err, docs) => {
-        if (err) console.log(err);
-        else {
-          if (docs) {
-            let userDetails = docs;
-            if (
-              userDetails["username"] == username &&
-              userDetails["password"] == hash
-            ) {
-              let sid = md5(hash + username);
-              if (exists_sid && sid != exists_sid) {
-                return res.status(400).send("Logout to login again");
-              } else {
-                // redis.hSet("sessions", sid, username);
-                // console.log(redis.hGetAll("sessions", sid));
-
-                console.log("cookie being set: ", sid);
-                res.cookie(cookieKey, sid, {
-                  maxAge: 3600 * 1000,
-                  sameSite: "None",
-                  httpOnly: true,
-                  secure: true,
-                });
-                // res.setHeader("Set-Cookie", sid);
-                Profile.findOne({ username: username }, (err, docs) => {
-                  if (err) {
-                    console.log(err);
-                  } else {
-                    if (docs) {
-                      sessionUser[sid] = username;
-                      let msg = {
-                        username: docs["username"],
-                        email: docs["email"],
-                        headline: docs["headline"],
-                        dob: docs["dob"],
-                        mobile: docs["mobile"],
-                        zipcode: docs["zipcode"],
-                        avatar: docs["avatar"],
-                        following: docs["following"],
-                        result: "success",
-                        cookie: sid,
-                      };
-                      res.send(msg);
-                    }
-                  }
-                });
-              }
-            } else {
-              let msg = { result: "Invalid credentials" };
-              res.send(msg);
-            }
-          } else {
-            res.send({ result: "User does not exist" });
-          }
-        }
-      });
-    });
-  })();
-};
-
-const changePassword = (req, res) => {
-  if (!req.cookies) {
-    return res.sendStatus(401);
+    await newUserProfile.save();
+    res.json({ message: "User created successfully" });
   }
-  let sid = req.cookies[cookieKey];
+});
 
-  let username = sessionUser[sid];
-  const new_password = req.body.password;
-  let salt = username + "lively";
-  let hash = md5(salt + new_password);
+module.exports = router;
 
-  if (!new_password) return res.status(400).send("New password not given");
+// const login = (req, res) => {
+//   console.log("logging in");
+//   let exists_sid = req.cookies[cookieKey];
 
-  User.updateOne({ username: username }, { password: hash }, (err, docs) => {
-    if (err) console.log(err);
-    else {
-      let msg = { username: username, result: "success" };
-      res.send(msg);
-    }
-  });
-};
+//   let username = req.body.username;
+//   let password = req.body.password;
 
-const logout = (req, res) => {
-  // if (!req.cookies) return res.sendStatus(401);
-  // let sid = req.cookies[cookieKey];
-  if (req.isAuthenticated()) {
-    req.logOut();
-  }
-  console.log("Clearing cookie");
-  // if (!req.cookies) return res.sendStatus(401);
+//   if (!username || !password)
+//     return res.status(400).send("Missing username or password");
 
-  res.clearCookie(cookieKey, { sameSite: "none", secure: true });
-  // delete sessionUser(sid);
-  let msg = "OK";
-  res.send(msg);
-  res.end();
-};
+//   let user = userObjs[username];
+//   console.log("Jack: ", user);
+//   (async () => {
+//     const connector = mongoose.connect(connectionString, {
+//       useNewUrlParser: true,
+//       useUnifiedTopology: true,
+//     });
 
-module.exports = (app) => {
-  app.post("/register", register);
-  app.post("/login", login);
-  app.use(isLoggedIn);
-  app.put("/password", changePassword);
-  app.put("/logout", logout);
-};
+//     await connector.then(() => {
+//       // salt the password
+//       // check if the username and password matches
+//       let salt = username + "lively";
+//       let hash = md5(salt + password);
+
+//       User.findOne({ username: username }, (err, docs) => {
+//         if (err) console.log(err);
+//         else {
+//           if (docs) {
+//             let userDetails = docs;
+//             if (
+//               userDetails["username"] == username &&
+//               userDetails["password"] == hash
+//             ) {
+//               let sid = md5(hash + username);
+//               if (exists_sid && sid != exists_sid) {
+//                 return res.status(400).send("Logout to login again");
+//               } else {
+//                 // redis.hSet("sessions", sid, username);
+//                 // console.log(redis.hGetAll("sessions", sid));
+
+//                 console.log("cookie being set: ", sid);
+//                 res.cookie(cookieKey, sid, {
+//                   maxAge: 3600 * 1000,
+//                   sameSite: "None",
+//                   httpOnly: true,
+//                   secure: true,
+//                 });
+//                 // res.setHeader("Set-Cookie", sid);
+//                 Profile.findOne({ username: username }, (err, docs) => {
+//                   if (err) {
+//                     console.log(err);
+//                   } else {
+//                     if (docs) {
+//                       sessionUser[sid] = username;
+//                       let msg = {
+//                         username: docs["username"],
+//                         email: docs["email"],
+//                         headline: docs["headline"],
+//                         dob: docs["dob"],
+//                         mobile: docs["mobile"],
+//                         zipcode: docs["zipcode"],
+//                         avatar: docs["avatar"],
+//                         following: docs["following"],
+//                         result: "success",
+//                         cookie: sid,
+//                       };
+//                       res.send(msg);
+//                     }
+//                   }
+//                 });
+//               }
+//             } else {
+//               let msg = { result: "Invalid credentials" };
+//               res.send(msg);
+//             }
+//           } else {
+//             res.send({ result: "User does not exist" });
+//           }
+//         }
+//       });
+//     });
+//   })();
+// };
+
+// console.log("heck 6");
+
+// const changePassword = (req, res) => {
+//   if (!req.cookies) {
+//     return res.sendStatus(401);
+//   }
+//   let sid = req.cookies[cookieKey];
+
+//   let username = sessionUser[sid];
+//   const new_password = req.body.password;
+//   let salt = username + "lively";
+//   let hash = md5(salt + new_password);
+
+//   if (!new_password) return res.status(400).send("New password not given");
+
+//   User.updateOne({ username: username }, { password: hash }, (err, docs) => {
+//     if (err) console.log(err);
+//     else {
+//       let msg = { username: username, result: "success" };
+//       res.send(msg);
+//     }
+//   });
+// };
+
+// const logout = (req, res) => {
+//   // if (!req.cookies) return res.sendStatus(401);
+//   // let sid = req.cookies[cookieKey];
+//   if (req.isAuthenticated()) {
+//     req.logOut();
+//   }
+//   console.log("Clearing cookie");
+//   // if (!req.cookies) return res.sendStatus(401);
+
+//   res.clearCookie(cookieKey, { sameSite: "none", secure: true });
+//   // delete sessionUser(sid);
+//   let msg = "OK";
+//   res.send(msg);
+//   res.end();
+// };
+
+// // module.exports = (app) => {
+// //   app.post("/register", register);
+// //   app.post("/login", login);
+// //   app.use(isLoggedIn);
+// //   app.put("/password", changePassword);
+// //   app.put("/logout", logout);
+// // };
