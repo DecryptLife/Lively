a8f5ba51f00fff365c790cffc653b0dcabad9a57
Benson Thomas <benson@Bensons-MacBook-Pro.local>
2024-08-04 15:20:11 -0500

    comments display fixed

--- lively_frontend/src/components/TitleBar/TitleBar.js
+++ lively_frontend/src/components/TitleBar/TitleBar.js
@@ -1,7 +1,19 @@
 import "./titlebar.css";
 import "../../styles/styles.css";
+import { useNavigate } from "react-router-dom";
 
 const TitleBar = () => {
+  const navigate = useNavigate("");
+
+  const handleNavigation = (page) => {
+    switch (page) {
+      case "profile":
+        navigate("/profile");
+        break;
+      default:
+        console.log("no page");
+    }
+  };
   return (
     <div className="title_container">
       <div className="flex-center title_container__left">
@@ -9,7 +21,10 @@ const TitleBar = () => {
         <div className="titlebar_item">
           <span>Home</span>
         </div>
-        <div className="titlebar_item">
+        <div
+          className="titlebar_item"
+          onClick={() => handleNavigation("profile")}
+        >
           <span>Profile</span>
         </div>
 
