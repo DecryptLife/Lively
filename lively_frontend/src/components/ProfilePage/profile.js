78696aeb967f2fd1effec0b5193acf464621d18b
Benson Thomas <benson@Bensons-MacBook-Pro.local>
2024-08-28 16:40:00 -0500

    cls fixed

--- lively_frontend/src/components/ProfilePage/profile.js
+++ lively_frontend/src/components/ProfilePage/profile.js
@@ -49,7 +49,7 @@ const Profile = () => {
 
   const handleImageSelect = async (e) => {
     try {
-      const [filename, image] = await transformImage(e);
+      const [_, image] = await transformImage(e);
 
       setUpdateDetails((prev) => ({
         ...prev,
